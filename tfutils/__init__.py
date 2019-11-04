d3b27630ae19952449051719f996676e6b5a12f1
supermario94123 <supermario94123@gmail.com>
2019-11-04 15:21:24 +0100

    docstrings

--- tfutils/__init__.py
+++ tfutils/__init__.py
@@ -3,13 +3,40 @@ import numpy as np
 
 
 def slice_img_around_mu(img, mu, slice_size):
-    """ extract rectangular slice around img with mean mu
-    :param img:
-    :param mu: in range [-1, 1]
-    :param slice_size: tuple of ints
-    :return: bn, n_part, slice_size[0] , slice_size[1], channel colour + n_part
-    """
+    """extract rectangular slice around img center point `mu`
+    
+    Parameters
+    ----------
+    img : Tensor
+        in range [-1, 1]. Shaped [N, H, W, C]
+    mu : Tensor
+        Batch of center points. Shaped [N, P, 2]
+    slice_size : Tuple of ints
+        height and width of slices to extract
+    
+    Returns
+    -------
+    image_slices
+        Tensor of extracted image slices. Shaped [N, slice_size[0], slice_size[1], P, C]
 
+    Examples
+    --------
+        mu = np.zeros((1, 1, 2), dtype=np.float32)
+        mu = tf.concat([mu, mu + 0.25], axis=1)
+        mut = tf.convert_to_tensor(mu)
+        imgt = astronaut(1)
+
+        slice_ = slice_img_around_mu(imgt, mut, (200, 200))
+
+        fig, ax = plt.subplots(2, 2)
+        ax = ax.ravel()
+        ax[0].imshow(np.squeeze(imgt)[...])
+        ax[0].set_title("Image")
+        ax[1].imshow(np.squeeze(slice_)[:, :, 0, :])
+        ax[1].set_title("slice")
+        ax[2].imshow(np.squeeze(slice_)[:, :, 1, :])
+        ax[2].set_title("slice")
+    """
     h, w = slice_size
     bn, img_h, img_w, c = img.get_shape().as_list()  # bn this actually 2bn now
     bn_2, nk, _ = mu.get_shape().as_list()
@@ -43,12 +70,47 @@ def slice_img_around_mu(img, mu, slice_size):
 
 def slice_img_with_mu_L_inv(img, mu, L_inv, scale=1.0, threshold=0.6):
     """slices images with ellipses centered around means (mu) and inverse L matrices.
-    :param img: batch of images [N, H, W, C]
-    :param mu:  batch of part means in range [-1, 1] : [N, P, 2]
-    :param L_inv: L_inverses [N, P, 2, 2]
-    :param scale: float scalar to scale L_inv with
-    :param threshold: threshold to scale
-    :return: slice [N, H, W, P, C]
+    
+    Parameters
+    ----------
+    img : Tensor
+        batch of images shaped [N, H, W, C]
+    mu : Tensor
+        batch of means in range [-1, 1]. shaped [N, P, 2]
+    L_inv : Tensor
+        batch of L_inverses [N, P, 2, 2]
+    scale : float, optional
+        scalar value to scale L_inv with, by default 1.0
+    threshold : float, optional
+        threshold of heatmap for cutoff, by default 0.6
+    
+    Returns
+    -------
+    sliced_image
+        Stack of sliced images. Shaped [N, H, W, P, C]
+
+    Examples
+    --------
+        mu = np.zeros((1, 1, 2), dtype=np.float32)
+        mu = tf.concat([mu, mu + 0.25], axis=1)
+        mut = tf.convert_to_tensor(mu)
+
+        L_invt = tf.convert_to_tensor(np.array([[2, 0], [0, 1]], dtype=np.float32))
+        L_invt = tf.expand_dims(L_invt, 0)
+        L_invt = tf.expand_dims(L_invt, 0)
+        L_invt = tf.concat([L_invt, 0.3 * L_invt], axis=1)
+
+        imgt = astronaut(1)
+        slice_ = slice_img_with_mu_L_inv(imgt, mut, L_invt, 3, 0.6)
+
+        fig, ax = plt.subplots(2, 2)
+        ax = ax.ravel()
+        ax[0].imshow(np.squeeze(imgt)[...])
+        ax[0].set_title("Image")
+        ax[1].imshow(np.squeeze(slice_)[:, :, 0, :])
+        ax[1].set_title("slice")
+        ax[2].imshow(np.squeeze(slice_)[:, :, 1, :])
+        ax[2].set_title("slice")        
     """
     bn, h, w, nc = img.get_shape().as_list()
     bn, nk, _ = mu.get_shape().as_list()
@@ -83,22 +145,75 @@ def slice_img_with_mu_L_inv(img, mu, L_inv, scale=1.0, threshold=0.6):
 
 
 def appearance_augmentation(
-    t, contrast_var=0.3, brightness_var=0.3, saturation_var=0.3, hue_var=0.3
+    image, contrast_var=0.3, brightness_var=0.3, saturation_var=0.3, hue_var=0.3
 ):
-    """t in range [0, 1]"""
-    t = tf.image.random_contrast(t, lower=1 - contrast_var, upper=1 + contrast_var)
-    t = tf.image.random_brightness(t, brightness_var)
-    t = tf.image.random_saturation(t, 1 - saturation_var, 1 + saturation_var)
-    t = tf.image.random_hue(t, max_delta=hue_var)
-    return t
+    """Apply a range of appearance augmentations to an image tensor.
+    
+    Parameters
+    ----------
+    image : Tensor
+        tensor of images. [N, H, W, C]
+    contrast_var : float, optional
+        maximum contrast variation, by default 0.3
+    brightness_var : float, optional
+        maximum brightness variation, by default 0.3
+    saturation_var : float, optional
+        maximum saturation variation, by default 0.3
+    hue_var : float, optional
+        maximum hue variation, by default 0.3
+    
+    Returns
+    -------
+    augmented:
+        Augmented image tensor shaped [N, H, W, C]
+        
+    """
+    augmented = tf.image.random_contrast(
+        image, lower=1 - contrast_var, upper=1 + contrast_var
+    )
+    augmented = tf.image.random_brightness(augmented, brightness_var)
+    augmented = tf.image.random_saturation(
+        augmented, 1 - saturation_var, 1 + saturation_var
+    )
+    augmented = tf.image.random_hue(augmented, max_delta=hue_var)
+    return augmented
 
 
 def augment_img_at_mask(img, mask, augm_function=appearance_augmentation):
-    """mask
-    :param img: batch of images [N, H, W, C]
-    :param mask: mask [N, H, W, P]
-    :return: slice [N, H, W, C]
-    # TODO: test
+    """Augment image only as locations specified by mask tensor
+    
+    Parameters
+    ----------
+    img : Tensor
+        image tensor shaped [N, H, W, C]
+    mask : Tensor
+        1 - hot mask tensor shaped [N, H, W, P]. Each slice along last axis is a boolean mask where to apply a certain augmentation
+    augm_function : callable, optional
+        function that yields augmented image tensors, by default appearance_augmentation
+    
+    Returns
+    -------
+    Tensor
+        Tensor with augmented images shaped [N, H, W, C]
+
+    Examples
+    --------
+        imgt = astronaut(1)
+        mask = np.zeros((1, 512, 512, 2), dtype=np.float32)
+        mask[:, :250, :, 0] = 1.0
+        mask[:, 250:, :, 1] = 1.0
+
+        a = augment_img_at_mask(imgt, tf.convert_to_tensor(mask))
+        fig, ax = plt.subplots(2, 2)
+        ax = ax.ravel()
+        ax[0].imshow(np.squeeze(imgt))
+        ax[0].set_title("image")
+        ax[1].imshow(np.squeeze(a[0, ...]))
+        ax[1].set_title("augmented")
+        ax[2].imshow(np.squeeze(mask[..., 0]), cmap=plt.cm.gray)
+        ax[2].set_title("mask 0")
+        ax[3].imshow(np.squeeze(mask[..., 1]), cmap=plt.cm.gray)
+        ax[3].set_title("mask 1")
     """
     blending_map = mask
     augmented = tf.stack(
@@ -110,3 +225,25 @@ def augment_img_at_mask(img, mask, augm_function=appearance_augmentation):
     )
     augmented *= tf.expand_dims(blending_map, axis=4)
     return tf.reduce_sum(augmented, axis=3, keep_dims=False)
+
+
+from skimage import data
+
+
+def astronaut(n_times):
+    """return skimage.data.astronaut() n_times stacked in a batch of images
+    
+    Parameters
+    ----------
+    n_times : int
+        how many times to stack along batch axis
+
+    Examples
+    --------
+        image = astronaut(4)
+        image.shape
+        >>> [4, 512, 512, 3]
+    """
+    image = data.astronaut() / 255.0
+    image = tf.to_float(image)
+    return tf.stack([image] * n_times, axis=0)
